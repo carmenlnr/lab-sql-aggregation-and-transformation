18029e1ef8a2dceeea31601398350e1673435b1f
carmenlnr <c.linares@hotmail.es>
2026-04-29 01:20:54 +0200

    Update sakila_aggregation.sql

--- sakila_aggregation.sql
+++ sakila_aggregation.sql
@@ -3,13 +3,13 @@ USE sakila;
 -- 1. Challenge #1
 -- 1.1 Duración min y max de las peliculas
 SELECT 
-    MAX(length) AS max_duration,
-    MIN(length) AS min_duration
+    MIN(length) AS min_duration,
+    MAX(length) AS max_duration
 FROM film;
 -- 1.2 Duración media en horas y minutos
 SELECT 
     FLOOR(AVG(length) / 60) AS avg_hours,
-    FLOOR(AVG(length) % 60) AS avg_minutes
+    ROUND(AVG(length) % 60) AS avg_minutes
 FROM film;
 
 -- 2. Insight de rental
@@ -17,17 +17,13 @@ FROM film;
 SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) AS days_operating
 FROM rental;
 -- 2.2 Info de alquileres con mes y dia de la semana
-SELECT 
-    rental_id,
-    rental_date,
+SELECT *,
     MONTH(rental_date) AS rental_month,
     DAYNAME(rental_date) AS rental_weekday
 FROM rental
 LIMIT 20;
 -- 2.3 Bonus, tipo de día: fin de semana y día laborable
-SELECT 
-    rental_id,
-    rental_date,
+SELECT *,
     DAYNAME(rental_date) AS rental_weekday,
     CASE 
         WHEN DAYNAME(rental_date) IN ('Saturday', 'Sunday') THEN 'weekend'
@@ -35,18 +31,56 @@ SELECT
     END AS day_type
 FROM rental
 LIMIT 20;
+
 -- 3. Titulos y duración de alquiler, NULL reemplazado por 'Not available'
 SELECT 
     title,
     IFNULL(rental_duration, 'Not Available') AS rental_duration
 FROM film
 ORDER BY title ASC;
+
 -- 4. bonus
 SELECT 
     CONCAT(first_name, ' ', last_name) AS full_name,
-    LEFT(email, 3) AS email_start
+    SUBSTRING(email, 1, 3) AS email_start
 FROM customer
 ORDER BY last_name ASC;
 
 -- Challenge 2
--- 1. 
+-- 1.1 Total peliculas lanzadas
+SELECT COUNT(film_id) AS total_films
+FROM film;
+-- 1.2 Numero peliculas por clasificacion
+SELECT 
+    rating,
+    COUNT(film_id) AS number_of_films
+FROM film
+GROUP BY rating;
+-- 1.3 Peliculas por clasificacion ordenadas de forma descendente
+SELECT 
+    rating,
+    COUNT(film_id) AS number_of_films
+FROM film
+GROUP BY rating
+ORDER BY number_of_films DESC;
+
+-- 2.1 Duracion media por clasificacion ordenada de mayor a menor
+SELECT 
+    rating,
+    ROUND(AVG(length), 2) AS mean_duration
+FROM film
+GROUP BY rating
+ORDER BY mean_duration DESC;
+-- 2.2 Clasificacion con duracion media mayor de 120 min
+SELECT 
+    rating,
+    ROUND(AVG(length), 2) AS mean_duration
+FROM film
+GROUP BY rating
+HAVING mean_duration > 120;
+
+-- 3. bonus
+SELECT last_name
+FROM actor
+GROUP BY last_name
+HAVING COUNT(last_name) = 1;
